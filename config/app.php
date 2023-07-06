c6bd74414d735b45d24b9ae747e77a0a365d8b57
Sumon Ahmed <bdsumon4u@gmail.com>
2023-07-06 07:56:07 +0600

    BugFix: Sail Server

--- config/app.php
+++ config/app.php
@@ -172,6 +172,7 @@ return [
         /*
          * Package Service Providers...
          */
+        \Hotash\LaravelMultiUi\UiServiceProvider::class,
 
         /*
          * Application Service Providers...
